d0908013641cbf549e2f449061e4236b18b6595f
Akinde Titilayo Tunmise <93851293+Akinde1@users.noreply.github.com>
2022-09-27 02:00:48 +0100

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/100-status_code.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# script to get status code of response
-curl -s "$1" -o /dev/null -w "%{http_code}"
--- 0x10-python-network_0/101-post_json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# script to send a Json POST req and display the response
-curl -s "$1" -X POST -H "Content-Type: application/json" -d @"$2"
